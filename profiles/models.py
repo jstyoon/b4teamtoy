cab94351247c4d28aca31eef1468c1c4b0cd34e5
이지훈 <jihunlee07@naver.com>
2023-05-11 20:00:15 +0900

    project upload

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_images', blank=True, null=True)
+    status = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'follower')
+
+    def __str__(self):
+        return f'{self.user.username} follows {self.follower.username}'
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ProfileForm
+from .models import Profile, Follower
 
-# Create your views here.
+@login_required
+def edit_profile(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '프로필이 업데이트 되었습니다.')
+            return redirect('profile', username=request.user.username)
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'profiles/edit_profile.html', {'form': form})
+
+def profile(request, username):
+    profile = get_object_or_404(Profile, user__username=username)
+    following = Follower.objects.filter(user=profile.user).count()
+    
+    #
